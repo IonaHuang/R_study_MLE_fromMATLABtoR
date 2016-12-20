bfd479dd83ad8994cf2978973c6d5495799bc981
IonaHuang <iona.huang01@gmail.com>
2016-12-20 10:31:33 +0800

    added a sample script
    contains multi-functions

---
+++ funcs.R
@@ -0,0 +1,15 @@
+f1 <- function() {
+  return("I'm func 1!");
+}
+
+f2 <- function() {
+  return("I'm func 2!");
+}
+
+f3 <- function() {
+  return("I'm func 3!");
+}
+
+f4 <- function() {
+  return("I'm func 4!");
+}
