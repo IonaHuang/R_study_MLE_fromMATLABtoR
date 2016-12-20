62d905adacc7c812a661b23f5eed09510eacfe8b
IonaHuang <iona.huang01@gmail.com>
2016-12-20 10:30:04 +0800

    A simple test

---
+++ step3a_m_calibration.R
@@ -0,0 +1,14 @@
+# This function is the main function of MLE calibration.
+# Author: Iona
+# Date: 15/12/2016
+
+# step3a_m_calibration<-function(groupArray,horizonArray,bUseRandInit,strTestingPath){
+step3a_m_calibration=function(){
+  # currentpath = getwd()
+  #setwd("")
+  
+  a = 3
+
+  return(a)
+  
+}
